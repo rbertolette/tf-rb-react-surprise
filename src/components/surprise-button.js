1ce9311f30954b321fa2b268af7c8f356a6c51a7
Reed Bertolette <reed@bertopro.com>
2018-08-14 15:47:39 -1000

    Reed's solution

--- src/components/surprise-button.js
+++ src/components/surprise-button.js
@@ -1,6 +1,32 @@
 import React from 'react';
+import SurpriseImage from './surprise-image';
 
-export default function SurpriseButton(props) {
-    return <button>Surprise!</button>;
+export default class SurpriseButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isClicked: false
+        }
+    }
+
+    setClicked(isClicked) {
+        this.setState({
+            isClicked
+        })
+    }
+
+
+    render() {
+        if (!this.state.isClicked) {
+            return (
+                <button type="button" onClick={() => this.setClicked(true)}>Surprise!</button>
+            );
+        }
+        else {
+            setTimeout(() => {
+                this.setClicked(false);
+            }, 6000);
+            return <SurpriseImage />;
+        }
+    }
 }
-
