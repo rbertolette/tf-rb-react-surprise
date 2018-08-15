1c6b014a1728aeebd0684d8e4be0b0ed6f4b8a23
Reed Bertolette <reed@bertopro.com>
2018-08-14 17:01:47 -1000

    Solution with timeout

--- src/components/surprise.js
+++ src/components/surprise.js
@@ -6,10 +6,30 @@ import SurpriseImage from './surprise-image';
 export default class Surprise extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            display: 'button'
+        }
     }
 
+    showSurprise() {
+        // set a timeout to reset the display to button
+        setTimeout(
+            () => this.setState({
+                display: 'button'
+            }),
+            6000 // 6 seconds
+        );
+        // now set the display to the image
+        this.setState({
+            display: 'surprise'
+        });
+    }
     render() {
-        // Show the button to start with
-        return <SurpriseButton />;
+        if (this.state.display === 'button') {
+            return <SurpriseButton onClick={e => this.showSurprise()} />;
+        }
+        else {
+            return <SurpriseImage />
+        }
     }
 }
